a596e3b0a55206fa508e61882fee5b3926cb29e4
alurmz9409 <alumil9409@ieselcaminas.org>
2022-12-15 08:25:19 +0100

    Añadido el archivo ej8.sh

---
+++ ej8.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Realizar un script utilizando el bucle for que muestre el siguiente
+#patrón:
+
+for (( a=1; a<=5; a++  ))
+do
+	for (( b=1; b<=$a; b++ ))
+		do
+			echo -e "$a \c"
+		done
+			echo -e "\n"
+done
+
