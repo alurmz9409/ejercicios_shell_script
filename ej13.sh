851e93410d4c21c235da29f6b9f617a1c76bedce
alurmz9409 <alumil9409@ieselcaminas.org>
2022-12-17 17:14:54 +0100

    Añadido el archivo ej13.sh

---
+++ ej13.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Haz un script que vaya dando la suma de todos los números que se
+#introduzca por teclado hasta que se introduzca un 0, en cuyo caso se
+#mostrará el último resultado y terminará el script.
+
+op=1
+res=0
+until [[ $op -eq 0 ]]; do
+	read -p "Introduce un número a sumar a $res: " op
+	echo -e "$res + $op = \c"
+	((res=$res+$op))
+	echo -e "$res\n"
+done
