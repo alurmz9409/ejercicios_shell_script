f0bd4126b3b96053e5b2998f2e50dbdc221eacab
alurmz9409 <alumil9409@ieselcaminas.org>
2022-12-17 17:46:31 +0100

    Añadido el archivo ej14.sh

---
+++ ej14.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Realizar un script que reciba como único parámetro el nombre de un
+#directorio, especificado mediante su nombre de ruta completo. El
+#programa debe mostrar un listado no recursivo de todas las entradas
+#contenidas en ese directorio, indicando para cada una de ellas si se trata
+#de un fichero o de un directorio. Al final, debe mostrarse un mensaje
+#indicando el número total de entradas procesadas.
+
+ls -lh $1
+echo -e "\n Número de entradas: "
+ls $1 | wc -l
