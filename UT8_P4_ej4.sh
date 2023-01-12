6f78ec204145fb1ab62df335b042d253032b3bfe
alurmz9409 <alumil9409@ieselcaminas.org>
2023-01-12 10:37:08 +0100

    Añadido el archivo UT8_P4_ej4.sh

--- UT8_P4_ej4.sh
+++ UT8_P4_ej4.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
 
-#guarde en el fichero /var/log/reinicios.txt la fecha de cada vez
-#que se reinicia el equipo (pista @reboot en crontab)
+#Modificar el script anterior de manera que permita leer los datos
+#personales de varios usuarios pertenecientes a un grupo desde un fichero 
+#de texto.
 
-#Características de reinicio
-last reboot -n 1 >> /var/log/reinicios.txt
+while IFS=, read -r op nombre a1 a2 grp
+do
+nom=${nombre:0:1}
+ap1=${a1:0:2}
+ap2=${a2:0:2}
+
+if [[ $grp != "" ]]; then
+   if [[ $op == "alta" ]]; then
+      useradd "alu${ap1}${ap2}${nom}"
+      groupadd $grp
+      usermod -a -G $grp "alu${ap1}${ap2}${nom}"
+      echo "Usuario alu${ap1}${ap2}${nom} creado y añadido al grupo $grp"
+   elif [[ $op == "baja" ]]; then
+       userdel "alu${ap1}${ap2}${nom}"
+       groupdel $grp
+       echo "Usuario alu${ap1}${ap2}${nom} y grupo $grp eliminados"
+    else
+       echo "Error. Formato incorrecto"
+    fi
+   else
+       if [[ $op = "alta" ]]; then
+          useradd "alu${ap1}${ap2}${nom}"
+          echo "Usuario alu${ap1}${ap2}${nom} creado correctamente"
+       elif [[ $op == "baja" ]]; then
+          userdel "alu${ap1}${ap2}${nom}"
+          echo "Usuario alu${ap1}${ap2}${nom} eliminado"
+       else
+          echo "Error. Formato incorrecto"
+       fi
+   fi
+fi
+done < usuarios.txt
